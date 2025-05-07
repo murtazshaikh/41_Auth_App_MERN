84a7f8a97f82a8d83368d1c43039d435146d978d
Murtaza Shaikh <95634721+murtazshaikh@users.noreply.github.com>
2025-05-07 09:56:58 +0530

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -27,7 +27,7 @@ function Home() {
 
   const fetchProducts = async () => {
     try {
-      const url = "http://localhost:8000/products";
+      const url = "https://auth-app-mern-api-six.vercel.app/products";
       const headers = {
         headers: {
           Authorization: localStorage.getItem("token") as string,
